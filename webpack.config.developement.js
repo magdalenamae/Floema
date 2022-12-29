408751a8f2009a3bc9cc71b76882e73929b01279
magdalena <@gmail.com>
2022-12-29 16:13:13 +1100

    first commit

---
+++ webpack.config.developement.js
@@ -0,0 +1,21 @@
+const { merge } = require('webpack-merge')
+const path = require('path')
+
+const config = require('./webpack.config')
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = merge(config, {
+  mode: 'development',
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    devMiddleware: {
+        writeToDisk: true,
+      }
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'public')
+  }
+})
